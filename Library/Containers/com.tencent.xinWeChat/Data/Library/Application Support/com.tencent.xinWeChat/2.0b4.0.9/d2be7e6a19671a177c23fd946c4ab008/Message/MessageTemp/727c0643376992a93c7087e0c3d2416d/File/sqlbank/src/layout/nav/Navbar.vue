4600df766847e2a621596db8c21f6f9988a4358f
piggymichelle <spotofsky@outlook.com>
2022-03-30 20:09:16 +0800

    hachathon2022

---
+++ Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/d2be7e6a19671a177c23fd946c4ab008/Message/MessageTemp/727c0643376992a93c7087e0c3d2416d/File/sqlbank/src/layout/nav/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="header"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+.header{
+  width: 100%;
+  height: 40px;
+  background-color: #42b983;
+}
+</style>
---
+++ Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/d2be7e6a19671a177c23fd946c4ab008/Message/MessageTemp/727c0643376992a93c7087e0c3d2416d/File/sqlbank/src/views/ChooseView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="ChooseView">
+    <div class="title">Select database</div>
+    <div class="btns">
+      <button @click="linkClick('sql1')">SQL1</button>
+      <button @click="linkClick('sql2')">SQL2</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChooseView",
+  data() {
+    return{
+
+    }
+  },
+  methods: {
+    linkClick(type) {
+      this.$router.push({
+        path: './home',
+        query: {
+          type: type
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ChooseView{
+  width: 100%;
+  /*height: 100vh;*/
+}
+.title{
+  width: 100%;
+  text-align: center;
+  font-family: 'Teko', cursive;
+  font-size: 6vw;
+  margin-top: 4vw;
+}
+.btns{
+  width: 100%;
+  text-align: center;
+  margin-top: 4vw;
+}
+.btns>button{
+  margin: 0 20px;
+  width: 200px;
+  font-family: 'Hanalei Fill', cursive;
+  font-size: 3vw;
+  background: white;
+  border-radius: 5px;
+}
+</style>
